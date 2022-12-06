dc1d566434411cde1bc9d6863849a4f2359bd440
Juanios CO <jggexito@gmail.com>
2022-12-06 11:15:32 -0600

    otro branch

--- otro.py
+++ otro.py
@@ -5,7 +5,7 @@ numeros =[49,21,52,54,56,45,43,29,5,12,40,50,13,26,19,25,33,51,48,15,16,32,24,17
 lista = []
 resultado =[]
 
-for x in range(32468436):
+for x in range(100):
     while len(lista) < 6:
         r1 = numeros[random.randint(0,27)]
         if not r1 in lista:
@@ -18,8 +18,10 @@ for x in range(32468436):
 print("")
 print("")'''
 
-print(resultado[random.randint(0,32468435)])
-print(resultado[random.randint(0,32468435)])
-print(resultado[random.randint(0,32468435)])
+print(resultado[random.randint(0,100)])
+print(resultado[random.randint(0,100)])
+print(resultado[random.randint(0,100)])
 
+
+print ('otro')
 
